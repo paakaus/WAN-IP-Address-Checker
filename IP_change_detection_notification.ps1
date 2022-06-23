f98bf9bf50c538747e5ddd85ce605fd63be67e01
paakaus <admin@adamsectech.com>
2022-06-23 15:22:49 +1000

    Updated

--- IP_change_detection_notification.ps1
+++ IP_change_detection_notification.ps1
@@ -1,8 +1,21 @@
-#Script to compare current IP with old IP and sends Slack notification if different (and do nothing if there was no change).
+#Script to compare current IP with old IP and sends webhook notification if different (and do nothing if there was no change).
 #We can put this as a scheduled task to run daily.
 #ScriptName: IP_change_detection_notification.ps1
-mkdir "C:\code"
-$slackWebhookUrl = "XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX" #put yours here
+$Directory = "C:\code"
+if (Test-Path $Directory) {
+   
+    Write-Host "Folder Exists"
+    # Perform Delete file from folder operation
+}
+else
+{
+  
+    #PowerShell Create directory if not exists
+    New-Item $Directory -ItemType Directory
+    Write-Host "Folder Created"
+}
+
+$WebhookUrl = "XXXXXXXXXXXXXXXX" #put webhook here
 $ipDetectionUrl = "https://wtfismyip.com/text"
 $IPAddFile = "C:\code\IP_change_detection_notification.dat" #absolute path to file that stores the old IP record
 $slackOutputFile = "C:\code\IP_change_detection_notification_Slack.txt"
@@ -25,7 +38,7 @@ $IP_old = Get-Content "$IPAddFile"
         Write-Host "$msg"               
         $body = $("{""text"":""$msg""}")
         Write-Host "$body"
-        Invoke-RestMethod -Uri $slackWebhookUrl -Method Post -ContentType 'application/json' -Body $body -OutFile $slackOutputFile
+        Invoke-RestMethod -Uri $WebhookUrl -Method Post -ContentType 'application/json' -Body $body -OutFile $slackOutputFile
 
         "Notification Sent"     
 
@@ -45,6 +58,3 @@ $IP_new |  Out-File $IPAddFile
 }
 $(get-date -f yyyy-MM-dd_HH_mm_ss)  |  Out-File $optionalDebuggingFile
 #Read-Host -Prompt "Press Enter to exit" #Comment out this line if this script will be run by a cron job. Otherwise, uncomment it so that you can see the results of the script in the console.
-
-#This script was adapted from https://gallery.technet.microsoft.com/scriptcenter/Detect-IP-address-change-aeb51118 by Satyajit
-To get the Task Scheduler working:
